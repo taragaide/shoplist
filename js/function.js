c273e5f02a1975c00ed35a53bbccf6b95963414d
taragaide <taragaide@yahoo.com>
2015-08-26 10:17:06 -0400

    jsnotworking

--- js/function.js
+++ js/function.js
@@ -43,10 +43,9 @@ function getInput() {
 
 function addItem(message) {
   $(".cross").hide(); // hiding the delete icon
-  var checkbox = "<td class=\"check\">" + "<input type=\"checkbox\" id=\"item" + id + "\" class=\"box\">" + "<label for=\"item" + id + "\" class=\"check-label\"></label></td>";
+  var checkbox = "<td class=\"check\">" + "<input type=\"checkbox\" id=\"item" + "\" class=\"box\">" + "<label for=\"item" + "\" class=\"check-label\"></label></td>";
   var content = "<td class=\"content\"><span>" + message + "</span></td>";
   var delIcon = "<td><img src=\"img/cross.png\" alt=\"cross\" class=\"cross\"></td>";
 
   $("tbody").append("<tr>" + checkbox + content + delIcon + "</tr>");
-  id++;
 }
